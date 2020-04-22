a3042af56adc5f09d19d8f72d17a7698240d0af8
yangzhu <yangzhu@onchain.com>
2020-04-22 14:51:53 +0800

    debug and something remind

---
+++ ont-test/utils/exmple.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	goSdk "github.com/ontio/ontology-go-sdk"
+	"github.com/ontio/ontology/common"
+	"github.com/ontio/ontology/common/log"
+)
+
+//txdata,err :=sdk.GetTransaction("1b72fa611f4dfe65efa2797051cd7b7143da2c0737cb6b30919360f03e20d91a")
+//log.Infof("txdata is %x ", txdata)
+//	Utils "github.com/mockyz/AutoTestGo/ont-test/utils"
+
+func BalanceOfONG(sdk *goSdk.OntologySdk, address common.Address) {
+	balance, err := sdk.Native.Ong.BalanceOf(address)
+	if err != nil {
+		log.Errorf("get account err: %s", err)
+		return
+	}
+	log.Infof("account %s is balance of %d", address.ToBase58(), balance)
+}
+
+func BalanceOfONT(sdk *goSdk.OntologySdk, address common.Address) {
+	balance, err := sdk.Native.Ont.BalanceOf(address)
+	if err != nil {
+		log.Errorf("get account err: %s", err)
+		return
+	}
+	log.Infof("account %s ONG  balance of %d", address.ToBase58(), balance)
+}
